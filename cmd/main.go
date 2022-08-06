b40c84072273681c13636efcf29ebd40ee196bf8
Mahmudur Rahman <raianmr@gmail.com>
2022-08-06 23:03:03 +0600

    ep 03

--- cmd/main.go
+++ cmd/main.go
@@ -8,40 +8,51 @@ import (
 	"os/signal"
 	"time"
 
-	"github.com/raianmr/learn-microservices-with-go/msa/handlers"
+	"github.com/raianmr/learn-microservices-with-go/handlers"
 )
 
 func main() {
-	l := log.New(os.Stdout, "api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
+
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+
+	// create the handlers
+	ph := handlers.NewProducts(l)
+
+	// create a new serve mux and register the handlers
 	sm := http.NewServeMux()
+	sm.Handle("/", ph)
 
-	sm.Handle("/hello", hh)
-	sm.Handle("/goodbye", gh)
-
-	s := &http.Server{
-		Addr:         ":9090",
-		Handler:      sm,
-		IdleTimeout:  120 * time.Second,
-		ReadTimeout:    1 * time.Second,
-		WriteTimeout:   1 * time.Second,
+	// create a new server
+	s := http.Server{
+		Addr:         ":9090",           // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
 
+	// start the server
 	go func() {
+		l.Println("Starting server on port 9090")
+
 		err := s.ListenAndServe()
 		if err != nil {
-			l.Fatal(err)
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
 		}
 	}()
 
-	sichan := make(chan os.Signal)
-	signal.Notify(sichan, os.Interrupt)
-	signal.Notify(sichan, os.Kill)
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
 
-	sig := <-sichan
-	l.Println("gracefully shutting down", sig)
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
 
-	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	s.Shutdown(tc)
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
